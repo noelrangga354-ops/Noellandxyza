74b5cb75ecf82e1d30e2d196042cdcc3febbd8bf
noelrangga354-ops <noelrangga354@gmail.com>
2026-09-18 01:32:52 +0700

    Update src.lua

--- src.lua
+++ src.lua
@@ -1,9 +1,5 @@
 -- 8
-local Load = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Libwtf/refs/heads/main/libload2.lua"))() 
-local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Advanced/refs/heads/main/gui/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Advanced/refs/heads/main/gui/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Advanced/refs/heads/main/gui/InterfaceManager.lua"))()
-
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/src/init.lua"))()
 
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
